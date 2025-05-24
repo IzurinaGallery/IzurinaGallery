ec699f4123cd366c19a7137b9aa0a8b7d0d25cac
IzurinaGallery <gallery.zuna@gmail.com>
2025-05-25 01:33:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -234,3 +234,32 @@ document.addEventListener('DOMContentLoaded', function() {
     // เริ่มต้นโดยการดึงข้อมูลกิจกรรมเมื่อหน้าเว็บโหลดเสร็จ
     fetchEvents();
 });
+
+
+// scrollbar //
+// ===============================================
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+    
+    //Handle scrollbar thumb drag
+    scrollbarThumb.addEventListener("mousedown", (e) => 
+    {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        //Update thumb position on mouse move
+        const handleMouseMove = (e) =>
+        {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetLeft;
+
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imageList.scrollLeft = scrollPosition;
